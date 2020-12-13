f25478bbc31dd70fbc1ae924daef901f7821052d
imcrisam <imcrisam@gmail.com>
2020-12-13 05:52:12 -0500

    funcionalidad terminada

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+Vue.use(VueRouter);
+const routes = [{
+  path: "/home",
+  name: "home",
+  component: Home,
+  meta: {
+    requiresAuth: true
   }
-]
-
+},
+{
+  path: "/",
+  name: "login",
+  component: () =>
+    import("../views/Login.vue")
+},
+];
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
-
-export default router
+});
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem("jwt") == null) {
+      next({
+        path: "/"
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Login">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <the-login>
+    </the-login>
+  </div>
+</template>
+
+<script>
+import TheLogin from '../components/auth/TheLogin.vue'
+export default {
+name: 'Login',
+  components: {
+    TheLogin
+    
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
